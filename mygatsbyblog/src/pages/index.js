3609898eca58ab53af0254559915779a040bc931
Chivisto <isaaccovarr@gmail.com>
2022-04-10 15:07:04 -0600

    Added Blog Posts and BootStrap

--- mygatsbyblog/src/pages/index.js
+++ mygatsbyblog/src/pages/index.js
@@ -1,16 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Seo title="Isaac Covarrubis Blog" />
+    <h1>The Bestest Blog Ever Made</h1>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -20,10 +17,8 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+      <Link to="/blog-1/">Go to blog post 1</Link> <br />
+      <Link to="/blog-2/">Go to blog post 2</Link> <br />
     </p>
   </Layout>
 )
